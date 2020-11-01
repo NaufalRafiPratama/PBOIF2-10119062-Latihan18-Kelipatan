04b2ece25f4859f6da984eda0ee456136a55e3e3
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-11-01 07:57:20 +0700

    Add files via upload

---
+++ PBOIF2-10119062-Latihan18-Kelipatan/src/pboif2/pkg10119062/latihan18/kelipatan/PBOIF210119062Latihan18Kelipatan.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119062.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * kelipatan
+ */
+public class PBOIF210119062Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int x =7;
+        int y =10;
+        int z =1;
+        int i =0;
+        
+        for (i=0;i<y;i++){
+        System.out.println(z*x*0.5);
+        z++;
+        }
+    }
+    
+}
